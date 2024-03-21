0f2fdec178048aa84bc9e22e6486f6d82ce2aa7e
Adam Mwaniki <adamndegwa@protonmail.com>
2024-03-21 17:30:06 +0300

    Completes arrow functions lab

---
+++ index.js
@@ -0,0 +1,18 @@
+//#1
+// arrow function that divides a by b 
+// first values are 2000 and 100
+const  divide = (a,b) => a/b;
+divide(2000,100);
+
+//#2
+//arrow function that makes a square exponent
+//value of x is 2
+const square = x => x**x
+square (2);
+
+//#3
+//arrow function called add
+//adds two values a and b
+//values are 3 and 4
+const add = (a,b) => a+b
+add (3,4)
